abd128b0b2caf7a963f4a20e668b2903deff2088
Piponga <djz@yandex.ru>
2018-08-27 23:31:40 +0300

    - created http server on port 8080 - added "start" command into package.json

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('learn nodejs');
+const http = require('http');
+const port = 8080;
+
+const requestHandler = (req, res) => {
+    console.log(req.uri);
+    res.end('Simple HTTP Server response');
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+   if (err) {
+       return console.log('error', err);
+   }
+    console.log(`server is listening on ${port}`);
+});
